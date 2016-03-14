0b9b7bbdbe0cacbb2abd291095e5240c0d6982f1
Alec <frstrtr@gmail.com>
2016-03-14 13:28:25 +0300

    Update RTMarkerTaker.cs
    added NaN checkups

--- Streaming/RTMarkerTaker.cs
+++ Streaming/RTMarkerTaker.cs
@@ -1,20 +1,19 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections.Generic;
-//#IISCI
 //Script for marker coordinates transfer to it's parent by marker name.
 
 namespace QualisysRealTime.Unity
 {
 
-	class MarkerTaker : MonoBehaviour
+	class RTMarkerTaker : MonoBehaviour
 	{
-	
-		public string markerName;
+
+		public string markerName = "Put QTM marker name here";
+		public Vector3 PositionOffset = new Vector3(0, 0, 0);
 
 		private List<LabeledMarker> markerData;
 		private RTClient rtClient;
 		private GameObject markerRoot;
-		private Vector3 markerPosition = new Vector3();
 		private bool streaming = false;
 
 		// Use this for initialization
@@ -27,7 +26,7 @@ namespace QualisysRealTime.Unity
 		// Update is called once per frame
 		void Update()
 		{
-			
+
 			if (rtClient == null) rtClient = RTClient.GetInstance();
 			if (rtClient.GetStreamingStatus() && !streaming)
 			{
@@ -37,22 +36,24 @@ namespace QualisysRealTime.Unity
 			{
 				streaming = false;
 			}
-			
+
 			//Seeking fo desired marker name
-			
+
 			markerData = rtClient.Markers;
 			if (markerData == null && markerData.Count == 0)
 				return;
-			for (int i = 0; i < markerData.Count; i++)
-			{
-					if (markerData[i].Label == markerName)
-					{
-						//Transfering marker position to parented object
-						
-						markerPosition = markerData[i].Position;
-						transform.position = markerPosition;
-					}
-			}
+            LabeledMarker marker = markerData.Find(x => x.Label == markerName);
+            if (marker!=null)
+            {
+                if (marker.Position != null)
+                {
+                    if(!float.IsNaN(marker.Position.x) && !float.IsNaN(marker.Position.y) && !float.IsNaN(marker.Position.z))
+                    {
+                       transform.position = marker.Position + PositionOffset;
+                    }
+                }
+            }
+            
 		}
 	}
 }
